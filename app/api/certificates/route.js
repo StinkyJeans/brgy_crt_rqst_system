37898ff8f4feb43b36c54cea30af251cf96b9abc
StinkyJeans <131233083+StinkyJeans@users.noreply.github.com>
2024-11-13 23:38:08 +0800

    Update route.js

--- app/api/certificates/route.js
+++ app/api/certificates/route.js
@@ -1,18 +1,18 @@
-// pages/api/certificates.js
+
 import connect from "@/app/utils/db";
-import Certificates from "@/models/Certificates"; // Assuming your model is named Certificates
+import Certificates from "@/models/Certificates"; 
 import { NextResponse } from "next/server";
 
-// Ensure that the database connection is established
+
 await connect();
 
-// Define the POST request handler
+
 export const POST = async (request) => {
-  // Extract purpose and documentTitle from the request body
+  
   const { firstName, email, purpose, documentTitle,status } = await request.json();
 
   try {
-    // Create a new instance of the Certificates model
+   
     const newCertificate = new Certificates({
       firstName,
       email,
@@ -21,13 +21,13 @@ export const POST = async (request) => {
       status: status || 'pending'
     });
 
-    // Save the new certificate to the database
+   
     await newCertificate.save();
 
-    // Respond with a success message
+   
     return new NextResponse("Certificate is registered", { status: 200 });
   } catch (err) {
-    // If an error occurs during saving, respond with an error message
+    
     return new NextResponse(err, { status: 500 });
   }
 };
