85f49764b7fda922aad81d3895882a5cc80062dd
StinkyJeans <131233083+StinkyJeans@users.noreply.github.com>
2024-11-13 23:40:45 +0800

    Update route.js

--- app/api/users/verify/route.js
+++ app/api/users/verify/route.js
@@ -1,6 +1,4 @@
-// pages/api/users/verify.js
-
-import User from "@/models/User"; // Import your User model or database connection
+import User from "@/models/User";
 import connect from "@/app/utils/db";
 import { NextResponse } from "next/server";
 
@@ -9,25 +7,24 @@ export const PUT = async (req) => {
   await connect();
 
   try {
-      // Extract the email from the request body
     const body = await req.json();
     const { email } = body;
     if (!email) {
      return new NextResponse("email is required", { status: 400 });
     }
   
-    // Find the user by their email in the database
+
     const user = await User.findOne({ email });
   
     if (!user) {
       return new NextResponse("user not found", { status: 400 });
     }
   
-    // Update the user's verification status (assuming you have a 'verified' field in your User schema)
+
     user.verified = true;
     await user.save();
   
-    // Send back a success response
+
     return new NextResponse("user is verified", { status: 200 });
   } catch (error) {
     
